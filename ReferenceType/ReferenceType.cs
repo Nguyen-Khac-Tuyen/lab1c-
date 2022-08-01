7fa0e759092e6aebc111960bacebbc5596214f41
Nguyễn Khắc Tuyên <97713665+Nguyen-Khac-Tuyen@users.noreply.github.com>
2022-08-01 09:06:03 +0700

    Update ReferenceType.cs

--- ReferenceType/ReferenceType.cs
+++ ReferenceType/ReferenceType.cs
@@ -1,15 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ReferenceType
+using System;
+class ReferenceType
 {
-    internal class Program
+    public int valueVal;
+}
+class TestReference
+{
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-        }
+        ReferenceType refer = new ReferenceType();//Create instance
+        refer.valueVal = 5;
+        Test(refer);//call function 
+        Console.WriteLine("The value of the variable is {0}", refer.valueVal);
+        Console.ReadLine();
+    }
+    static void Test(ReferenceType refer)
+    {
+        int temp = 5;
+        refer.valueVal = temp * 2;
     }
 }
