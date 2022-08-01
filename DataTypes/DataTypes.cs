b4172190616ef5639ed63cc899d257a5ba1e8718
Nguyễn Khắc Tuyên <97713665+Nguyen-Khac-Tuyen@users.noreply.github.com>
2022-08-01 09:03:54 +0700

    Update DataTypes.cs

--- DataTypes/DataTypes.cs
+++ DataTypes/DataTypes.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,10 +6,21 @@ using System.Threading.Tasks;
 
 namespace DataTypes
 {
-    internal class Program
+    internal class DataTypes
     {
         static void Main(string[] args)
         {
+            int intVal;
+            double dblVal;
+            string strVal;
+            intVal = 10;
+            dblVal = 3.142;
+            strVal = "Fpt Aptech";
+            Console.WriteLine("{0} is an integer value", intVal);
+            Console.WriteLine("{0} is an double value", dblVal);
+            Console.WriteLine("{0} is an string", strVal);
+            Console.ReadLine();
         }
+
     }
 }
